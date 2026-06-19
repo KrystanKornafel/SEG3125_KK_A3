f075e583ba491a8640ee2de8aa1dce576da8f29c
Krystan Kornafel <Korn0018@algonquinlive.com>
2026-06-18 21:20:23 -0400

    Adding the initial source code to be worked on

---
+++ public/MemoryGame.jsx
@@ -0,0 +1,24 @@
+import './MemoryGame.css'
+
+//======================== Memory Game Page ====================================================================================
+// Name: Krystan Kornafel
+// Assignment: SEG3125 Assignment 2
+// Description: This will be used in the future to generate a UI for a game application. Stay tuned!
+
+
+function MemoryGame() {
+  return (
+    <div className="container mt-5">
+        <div className="mb-5">
+          <div class="border rounded p-3 mb-4" style="background-color: #8de5e5; border: 10px solid #215689;">
+            <h1>Memory Game Fun!</h1>
+            <div class="border rounded p-3 mb-4" style="background-color: #c1eef4;">
+              <p>We will have content coming soon! Stay tuned for some fun games!</p>
+          </div>
+        </div>
+    </div>
+</div>
+  );
+}
+
+export default MemoryGame;
